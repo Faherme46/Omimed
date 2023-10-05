2c9276dfe2d1d46db522d75b1e14befa2d5ab8c7
DRAGO763 <sierragomezdavid@gmail.com>
2023-10-05 00:44:22 -0500

    hice cambios al tv

---
+++ resources/views/tv.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/tv.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TELEVISOR</title>
+</head>
+<body>
+
+<h1 align="center" >PANTALLA</h1>
+
+<div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      <H2>PACIENTES</H2>
+    </div>
+    <div> 
+    <table class="table">
+  <thead >
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombres</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Consultorio</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <th scope="row" >1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry the Bird</td>
+      <td>Thornton</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Larry the Bird</td>
+      <td>Thornton</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Larry the Bird</td>
+      <td>Thornton</td>
+      <td>7</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    <div class="col">
+    <H2>PUBLICIDAD</H2>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
