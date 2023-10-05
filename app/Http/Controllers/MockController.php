5f29cc59c32453329265ff37907a37815a6f8f10
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-10-05 01:57:25 -0500

    Conexion a la BD

---
+++ app/Http/Controllers/MockController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+class MockController{
+
+    public function getData($field){
+        // URL de la API que deseas consumir
+        $apiUrl = 'https://6287d9a77864d2883e8dca86.mockapi.io/Users';
+
+        // Realizar la solicitud GET a la API
+        $response = file_get_contents($apiUrl);
+
+        // Verificar si se recibió una respuesta
+        if ($response === false) {
+            die('Error al obtener los datos de la API.');
+        }
+
+        // Decodificar la respuesta JSON (o XML u otro formato) en un arreglo asociativo
+        $data = json_decode($response, true);
+        $dataComplete=$data[0];
+        $arrayDeseado=$dataComplete[$field];
+
+
+        return $arrayDeseado;
+}
+
+}
--- resources/views/login.php
+++ resources/views/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -41,4 +41,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
-</html>
+</html> -->
+
+
+<?php
+
+use App\Http\Controllers\MockController;
+use PhpParser\Node\Expr\Cast\Object_;
+
+
+    $controller= new MockController();
+    $dataUsers=$controller->getData("Users");
+
+    print_r($dataUsers);
+
+
+
